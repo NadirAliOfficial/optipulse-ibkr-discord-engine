7ffbdbf5b34b3f8033521da22979c6c6bd1fd7dd
NadirAliOffical <nadiralikhanofficial1@gmail.com>
2026-02-23 21:39:04 +0500

    SHOP to QQQ

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ load_dotenv()
 IB_HOST = os.getenv("IB_HOST", "127.0.0.1")
 IB_PORT = int(os.getenv("IB_PORT", "4002"))
 IB_CLIENT_ID = int(os.getenv("IB_CLIENT_ID", "1"))
-SYMBOL = os.getenv("SYMBOL", "SHOP")
+SYMBOL = os.getenv("SYMBOL", "QQQ")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "")
 
 TIMEFRAMES = {
